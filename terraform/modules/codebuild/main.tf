4c54d068f48189d0e60e556650592777f63c3cfa
Anis Merchant <anisxmerchant@gmail.com>
2026-02-10 21:15:34 -0500

    fix: parameterize CodeBuild project name to allow multiple instances
    - use project_name variable in CodeBuild resource
    - prevent naming collisions between backend and frontend builds
    - enable reuse of CodeBuild module for multiple services

--- terraform/modules/codebuild/main.tf
+++ terraform/modules/codebuild/main.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "this" {
-  name         = "microservices-with-codepipeline-and-ecs"
+  name         = var.project_name
   service_role = var.role_arn
 
   artifacts {
